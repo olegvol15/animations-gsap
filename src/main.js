da3b10ad40d0771b66ac2827d1fe435bbd017406
Oleg_Volostnykh <olegbasketball15@gmail.com>
2026-08-08 22:22:51 +0200

    style: styles for hero and container with the inter font

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import "./styles/main.scss";
+import '@fontsource-variable/inter';
 
 gsap.registerPlugin(ScrollTrigger);
